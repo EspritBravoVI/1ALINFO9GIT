7640d44814665bf05aaaa3546d30a7ea2c5b80e0
unknown <mohamedali.dellai@value.com.tn>
2023-03-17 21:49:22 +0100

    definir supp employee et afficher departement

--- map/src/main/java/org/example/SocieteHashMap.java
+++ map/src/main/java/org/example/SocieteHashMap.java
@@ -1,50 +1,53 @@
 package org.example;
 
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 public class SocieteHashMap implements InterfaceSociete {
 
-    HashMap<Employe, Departement> empdepmap = new HashMap<>();
+  HashMap<Employe, Departement> empdepmap = new HashMap<>();
 
 
-    @Override
-    public void ajouterEmployeDepartement(Employe e, Departement d) {
-        empdepmap.put(e, d);
+  @Override
+  public void ajouterEmployeDepartement(Employe e, Departement d) {
+    empdepmap.put(e, d);
+  }
+
+  @Override
+  public void supprimerEmploye(Employe e) {
+    empdepmap.remove(e);
+  }
+
+  @Override
+  public void afficherLesEmployesLeursDepartements() {
+    for (Map.Entry<Employe, Departement> mp : empdepmap.entrySet()) {
+      System.out.println(mp.getKey());
+      System.out.println(mp.getValue());
     }
+  }
 
-    @Override
-    public void supprimerEmploye(Employe e) {
+  @Override
+  public void afficherLesEmployes() {
 
-    }
+  }
 
-    @Override
-    public void afficherLesEmployesLeursDepartements() {
+  @Override
+  public void afficherLesDepartements() {
 
-    }
+  }
 
-    @Override
-    public void afficherLesEmployes() {
+  @Override
+  public void afficherDepartement(Employe e) {
 
-    }
+  }
 
-    @Override
-    public void afficherLesDepartements() {
+  @Override
+  public boolean rechercherEmploye(Employe e) {
+    return false;
+  }
 
-    }
-
-    @Override
-    public void afficherDepartement(Employe e) {
-
-    }
-
-    @Override
-    public boolean rechercherEmploye(Employe e) {
-        return false;
-    }
-
-    @Override
-    public boolean rechercherDepartement(Departement e) {
-        return false;
-    }
+  @Override
+  public boolean rechercherDepartement(Departement e) {
+    return false;
+  }
 }
